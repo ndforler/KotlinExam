980eb819ff71dde60b26af1df987b0bdbacbe4dd
ndforler <ndforler@lakeheadu.ca>
2021-04-24 13:12:21 -0400

    Initial commit

---
+++ app/src/main/java/com/example/nickforler1109405final/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nickforler1109405final
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
